6f2aa97e165f6896672362024622d00bf632c8da
RicardoMartins07 <rm45555@gmail.com>
2020-04-30 23:18:49 +0100

    AntiPatterns Found

--- Test/testeantipatterns.java
+++ Test/testeantipatterns.java
@@ -14,14 +14,18 @@ public class testeantipatterns {
     static void initialize(){
     }
 
+    //the liar
     @Test
     public void RegistoRequisicao(){
         livraria = new Livraria("Bertrand","Guimaraes");
         cliente = new Cliente(1, 21 ,"Paulo", "Guimaraes");
         livro = new Livro(1,"O cavaleiro da Dinamarca","PortoEditora", livraria);
         livraria.ReguisitarLivro(livro, cliente);
+
     }
 
+    //Test-per-Method
+    //Dodger
     @Test
     public void nomecliente(){
         cliente = new Cliente(1, 21 ,"Paulo", "Guimaraes");
@@ -29,6 +33,7 @@ public class testeantipatterns {
         assertEquals("Paulito", cliente.getNome());
     }
 
+    //secret catcher
     @Test
     public void verificadisponibilidadelivro(){
         livraria = new Livraria("Bertrand","Guimaraes");
@@ -37,6 +42,8 @@ public class testeantipatterns {
         assertEquals(1,livraria.ProcuraLivro(livro));
     }
 
+    //Enumerator
+    //FreeRide
     @Test
     public void testXPTO(){
         livraria = new Livraria("Bertrand","Guimaraes");
